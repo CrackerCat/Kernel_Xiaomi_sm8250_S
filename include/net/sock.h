fd69cbb48489853b4c379168699cdd398df7ac22
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-06-01 07:57:02 +0200

    bpf: Get the comm hash of the socket process stored inside sk buffer
    Change-Id: I47333f0e87e8dd6daf3ef00e328bcae2518b7e20
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -472,6 +472,12 @@ struct sock {
 	u32			sk_ack_backlog;
 	u32			sk_max_ack_backlog;
 	kuid_t			sk_uid;
+#if IS_ENABLED(CONFIG_DEBUG_SPINLOCK) || IS_ENABLED(CONFIG_DEBUG_LOCK_ALLOC)
+	spinlock_t		sk_peer_lock;
+#else
+	/* sk_peer_lock is in the ANDROID_KABI_RESERVE(1) field below */
+#endif
+	pid_t			pid_num;
 	struct pid		*sk_peer_pid;
 	const struct cred	*sk_peer_cred;
 	long			sk_rcvtimeo;
