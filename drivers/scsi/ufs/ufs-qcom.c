af18efeaa3303520cf12395a0a76c750a76132c5
TheVoyager0777 <88270212+TheVoyager0777@users.noreply.github.com>
2022-08-06 13:15:04 +0800

    scsi: ufs: force lpm on To fix the extra power consumption on umi Signed-off-by: TheVoyager0777 <88270212+TheVoyager0777@users.noreply.github.com>

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -2067,7 +2067,7 @@ static void ufs_qcom_parse_lpm(struct ufs_qcom_host *host)
 {
 	struct device_node *node = host->hba->dev->of_node;
 
-	host->disable_lpm = of_property_read_bool(node, "qcom,disable-lpm");
+	host->disable_lpm = false;
 	if (host->disable_lpm)
 		pr_info("%s: will disable all LPM modes\n", __func__);
 }
